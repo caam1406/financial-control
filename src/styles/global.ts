4112da553cef8e3bc24f70266e9f9ce31840bd1c
Carlos Matos <caam1406@gmail.com>
2022-05-26 20:41:46 -0300

    style: created base style

---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+:root {
+    --background-primary: #E1E2E1;
+    --background-secundary: #F5F5F6;
+    --color-primary: #004d40;
+    --green-light: #39796b;
+    --green-dark: #01251A;
+    --text-tile: #ffffff;
+}
+html {
+    @media (max-width: 1080px){
+        font-size: 93.75%; // 93.75% = 15px
+    }
+    @media(max-width: 720px){
+        font-size: 87.5%; // 87.5% = 14px
+    }
+}
+button {
+    cursor: pointer;
+}
+[disable]{
+    opacity: 0.6;
+    cursor: not-allowed;
+}
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    background: var(--background-primary);
+    -webkit-font-smoothing: antialiased; //Fix font details in chrome browser baseds
+}
+`;
