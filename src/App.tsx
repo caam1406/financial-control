cac1f84cbf24677156ff1762ad70f49f49517e13
Carlos Matos <caam1406@gmail.com>
2022-05-27 13:15:29 -0300

    feat: added dashboard component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/header/header";
+import { Dashboard } from "./components/dashboard";
 
 
 export function App() {
   return (
     <div className="App">
       <Header/>
+      <Dashboard/>
     </div>
   );
 }
