5756769026c03e99193039c85f3c2524d2411c56
Carlos Matos <caam1406@gmail.com>
2022-05-27 10:58:40 -0300

    feat: added font (google popins) to document

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -35,4 +35,11 @@ body {
     background: var(--background-primary);
     -webkit-font-smoothing: antialiased; //Fix font details in chrome browser baseds
 }
+body, input, button, textarea {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+}
+h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+}
 `;
