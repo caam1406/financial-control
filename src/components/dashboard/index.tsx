1b4c0352626b2567c29298f5ea4379fb638b8e71
Carlos Matos <caam1406@gmail.com>
2022-05-27 13:38:54 -0300

    refactor: added summary component as factory

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,11 +1,16 @@
 import { Summary } from "../summary";
 import { Container } from "./styled";
+import income from "../../assets/income.svg";
+import outcome from "../../assets/outcome.svg";
+import total from "../../assets/total.svg";
 
 export const Dashboard = () => {
     return(
     <>
     <Container>
-        <Summary title="Entradas" value={1000.00} />
+        <Summary title="Income" value={1000.00} image={income} alt="Income Money" />
+        <Summary title="Outcome" value={500.00} image={outcome} alt="Outcome Money" />
+        <Summary title="Total" value={500.00} image={total} alt="Total" />
     </Container>
     </>
     );
