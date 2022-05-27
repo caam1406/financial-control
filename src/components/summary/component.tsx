5f394fcd01aec2922dd77e0121ef875d1161b192
Carlos Matos <caam1406@gmail.com>
2022-05-27 14:18:41 -0300

    refactor: added class option

--- src/components/summary/component.tsx
+++ src/components/summary/component.tsx
@@ -2,7 +2,7 @@ import { ISummary } from "../../interfaces/summary.I";
 
 export const SummaryComponent = (props: ISummary) => {
   return (
-    <div>
+    <div className={props.className}>
       <header>
         <p>{props.title}</p>
         <img src={props.image} alt={props.alt} />
