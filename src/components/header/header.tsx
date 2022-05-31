1775ad405d656232f46a7e4361dcdc4b4c19d942
Carlos Matos <caam1406@gmail.com>
2022-05-31 20:22:53 -0300

    feat: called modal in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,23 @@
-import logo from '../../assets/logo.svg';
-import { Container, Content } from './styled';
+import React from "react";
+import logo from "../../assets/logo.svg";
+import { ModalComponent } from "./modal";
+import { Container, Content } from "./styled";
 
 export const Header = () => {
- return(<Container>
-     <Content>
-     <img src={logo} alt="Financial Control Logo" />
-     <button type="button">Nova Transação</button>
-     </Content>
- </Container>
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
- );
-}
+  const handleOpenModal = () => {
+    setIsOpen(!modalIsOpen);
+  };
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="Financial Control Logo" />
+        <button type="button" onClick={handleOpenModal}>
+          Nova Transação
+        </button>
+      </Content>
+      <ModalComponent open={modalIsOpen} setOpen={handleOpenModal} />
+    </Container>
+  );
+};
