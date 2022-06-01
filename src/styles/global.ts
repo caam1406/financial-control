3b656a3ac1283eb10fb757c221b7bfd23317bb18
Carlos Matos <caam1406@gmail.com>
2022-06-01 09:04:14 -0300

    style: added modal css options

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -42,4 +42,23 @@ body, input, button, textarea {
 h1, h2, h3, h4, h5, h6, strong {
     font-weight: 600;
 }
+.modal-overlay {
+    background: rgba(0,0,0,0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background-secundary);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem;
+}
 `;
