596824dcd2f560b06147704720d6e1a19c735afa
Carlos Matos <caam1406@gmail.com>
2022-06-02 10:22:30 -0300

    style: improve modal aparence

--- src/modal/index.tsx
+++ src/modal/index.tsx
@@ -1,12 +1,10 @@
 import Modal from "react-modal";
 import { IModalProps } from "../interfaces/modalProps.I";
 import { Container } from "./style";
+import close from "../assets/close.svg";
 
 export const ModalComponent = (props: IModalProps) => {
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    //  subtitle.style.color = "#f00";
-  }
+  function afterOpenModal() {}
   Modal.setAppElement("#root"); //Fix "Warning: react-modal: App element is not defined"
   return (
     <Modal
@@ -17,12 +15,14 @@ export const ModalComponent = (props: IModalProps) => {
       className="modal-content"
     >
       <Container>
-        {/* <button onClick={props.setOpen}>close</button> */}
-        <h1>Add Value</h1>
+        <button className="closeButton" onClick={props.setOpen}>
+          <img src={close} alt="close" />
+        </button>
+        <h1>ADD TRANSACTION</h1>
         <input placeholder="Title" />
         <input placeholder="Value" type="number" />
         <input placeholder="Category" />
-        <button type="submit">Add</button>
+        <button type="submit">CREATE</button>
       </Container>
     </Modal>
   );
--- src/modal/style.ts
+++ src/modal/style.ts
@@ -1,3 +1,52 @@
 import styled from "styled-components";
 
-export const Container = styled.form``;
+export const Container = styled.form`
+  h1 {
+    color: var(--color-primary);
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+  input {
+    width: 100%;
+    padding: 0.5rem;
+    height: 4rem;
+    border-radius: 0.25rem;
+    background: var(--background-primary);
+    font-weight: 400;
+    font-size: 1rem;
+    &::placeholder {
+      color: var(--green-light);
+    }
+    & + input {
+      // when you have more than one input create a space between them
+      margin-top: 1rem;
+    }
+  }
+  button[type="submit"] {
+    font-weight: 600;
+    width: 100%;
+    padding: 0.1.5rem;
+    height: 4rem;
+    background: var(--green-light);
+    color: var(--text-title);
+    border-radius: 0.25rem;
+    border: 0;
+    font-size: 1rem;
+    margin-top: 1.5rem;
+    &:hover {
+      filter: brightness(0.5);
+    }
+    transition: 0.2ms;
+  }
+  .closeButton {
+    border: 0;
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    background: transparent;
+    transition: 2ms;
+    &:hover {
+      filter: brightness(0.5);
+    }
+  }
+`;
