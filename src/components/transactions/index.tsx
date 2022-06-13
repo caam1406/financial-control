c8f380dee66e504465dcceb8c2861c78a0cf6062
Carlos Matos <caam1406@gmail.com>
2022-06-13 17:33:46 -0300

    style: formated date and value to pt-BR

--- src/components/transactions/index.tsx
+++ src/components/transactions/index.tsx
@@ -28,9 +28,18 @@ export const Transactions = () => {
             return (
               <tr key={transaction.id}>
                 <td>{transaction.title}</td>
-                <td className={transaction.type}>{transaction.type}</td>
+                <td className={transaction.type}>
+                  {new Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  }).format(transaction.amount)}
+                </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>
+                  {new Intl.DateTimeFormat("pt-BR").format(
+                    new Date(transaction.createdAt)
+                  )}
+                </td>
               </tr>
             );
           })}
